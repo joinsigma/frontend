39d960be75fce81321823cd633dd903a73ab1763
Danish Rashidin <danishrashidin@gmail.com>
2023-04-20 13:24:24 +0800

    add assignment_1

---
+++ module_5/assignment_1/src/components/AppContainer.vue
@@ -0,0 +1,16 @@
+<script>
+import ProductForm from './ProductForm.vue';
+
+export default {
+    components: { ProductForm },
+    methods: {
+        receiveEmitagain(value) {
+            console.log(value)
+        }
+    }
+}
+</script>
+
+<template>
+    <ProductForm @message="receiveEmitagain"></ProductForm>
+</template>
---
+++ module_5/assignment_1/src/components/ProductInput.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    emits: ["message"],
+    data() {
+        return {
+            message: "hello"
+        }
+    },
+    methods: {
+        emitData() {
+            let message = this.message
+            this.$emit('message', message)
+        }
+    }
+}
+</script>
+
+<template>
+    <input type="text" v-model="message" /><br>
+    <button @click="emitData">Emit Data</button>
+</template>
